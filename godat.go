9ebd0d9e49f145891443b0f9bd635dee58f934bf
guotie <guotie.9@gmail.com>
2014-09-24 18:53:41 +0800

    nope

--- godat.go
+++ godat.go
@@ -3,6 +3,7 @@ package godat
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strings"
 )
 
@@ -12,6 +13,8 @@ import (
 var (
 	minReserve      int     = 5
 	maxReserve      int     = 64
+	initArrayLen    int     = 1024
+	maxArrayLen     int     = 2 ^ 20
 	defReserveRatio float32 = 1.1
 )
 
@@ -24,12 +27,32 @@ type GoDat struct {
 	auxiliary map[rune]int
 	// patterns
 	pats []string
+	// 下一个将要使用的位置
+	nextPos int
+	idles   int
 	// options
 	nocase    bool // 区分大小写
 	ascii     bool // 仅处理ascii
 	minResv   int
 	maxResv   int
+	maxLen    int
 	resvRatio float32
+}
+
+// 创建双数组
+func CreateGoDat(pats []string, opts map[string]interface{}) (gd *GoDat, err error) {
+	gd.SetOptions(opts)
+	if gd.ascii {
+		for _, pat := range pats {
+			if err = gd.add(pat); err != nil {
+				log.Printf("CreateGoDat: pattern %s is invalid, maybe it contains non-ascii characters, ignored.\n", pat)
+			}
+		}
+	} else {
+		gd.pats = pats
+	}
+	err = gd.Build()
+	return
 }
 
 // dump godat
@@ -63,6 +86,7 @@ func (gd *GoDat) SetOptions(opts map[string]interface{}) {
 
 	gd.minResv = minReserve
 	gd.maxResv = maxReserve
+	gd.maxLen = maxArrayLen
 	gd.resvRatio = defReserveRatio
 
 	for key, v := range opts {
@@ -108,32 +132,108 @@ func (gd *GoDat) SetOptions(opts map[string]interface{}) {
 	}
 }
 
-// 创建
-func (gd *GoDat) Build() (err error) {
-	return
+// 扩大数组长度
+func (gd *GoDat) extend() (err error) {
+	length := len(gd.base) * 2
+	if length > gd.maxLen {
+		err = fmt.Errorf("Array cannot exceed dat's maxLen: %d", gd.maxLen)
+		return
+	}
+
+	base := make([]int, length)
+	check := make([]int, length)
+
+	copy(base, gd.base)
+	copy(check, gd.check)
+
+	gd.base = base
+	gd.check = check
+
+	return nil
 }
 
-// 全自动创建过程
-func CreateGoDat(pats []string, opts map[string]interface{}) (gd *GoDat, err error) {
-	gd.SetOptions(opts)
-	if gd.ascii {
-		for _, pat := range pats {
-			if err = gd.addPattern(pat); err != nil {
-				log.Printf("CreateGoDat: pattern %s is invalid, maybe it contains non-ascii characters, ignored.\n", pat)
+func (gd *GoDat) buildAux() {
+	if gd.auxiliary == nil {
+		gd.auxiliary = make(map[rune]int)
+	}
+
+	// 字符序号从1开始
+	chs := 1
+
+	for _, pat := range gd.pats {
+		for _, ch := range pat {
+			if _, ok := gd.auxiliary[ch]; !ok {
+				gd.auxiliary[ch] = chs
+				chs++
 			}
 		}
-	} else {
-		gd.pats = pats
 	}
-	err = gd.Build()
+}
+
+func abs(i int) int {
+	if i == 0 {
+		return 0
+	}
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
+// 创建
+func (gd *GoDat) Build() (err error) {
+	gd.base = make([]int, initArrayLen)
+	gd.check = make([]int, initArrayLen)
+	gd.auxiliary = make(map[rune]int)
+
+	// 创建字符辅助表
+	gd.buildAux()
+
+	// 对pattern排序, 节省空间
+	sort.Sort(sort.StringSlice(gd.pats))
+
+	// 第一个非首字母将要插入的位置
+	gd.nextPos = len(gd.pats)
+
 	return
 }
 
-// 动态增加一个模式
-func (gd *GoDat) AddPattern(pat string) {
+// 获取s的下一个字符r是否存在，存在返回下标，不存在返回-1
+func (gd *GoDat) nextState(s, c int) int {
+	// gd.check[0] should always be 0
+	if gd.check[s] < 0 {
+		return -1
+	}
+
+	// 检查状态转移表
+	// c := gd.auxiliary[r]
+	t := gd.base[s] + c
+	if gd.check[t] != s {
+		return -1
+	}
+
+	return t
+}
+
+// 增加一个模式
+func (gd *GoDat) buildPattern(pat string) {
+	arrLen := len(gd.base)
+	patLen := len(pat)
+
+	for i, r := range pat {
+
+	}
+}
+
+// 无冲突版本
+// 实现原理:
+//   1 按列插入
+func (gd *GoDat) buildWithoutConflict() {
 
 }
 
-func (gd *GoDat) Match(noodle string) bool {
-
+// 匹配
+//
+func (gd *GoDat) Match(noodle string, opt ...int) bool {
+	return false
 }
